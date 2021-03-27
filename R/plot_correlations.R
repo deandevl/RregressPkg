7d312622358af4bfb3e10b3c22b7d504a9e5af7e
Rick <deanr3@bardstown.com>
2021-03-27 07:05:07 -0400

    modified plot_correlations

--- R/plot_correlations.R
+++ R/plot_correlations.R
@@ -1,10 +1,11 @@
-#' Function produces a heatmap plot of a correlation matrix.
+#' Function computes simple Pearson correlations between variables of a data frame/matrix along with a heatmap plot.
 #'
-#' @description With the submission of a correlation matrix, the function returns a ggplot2 plot object that takes the form
+#' @description The data frame/matrix should have observations along the rows without NA values
+#'  and the columns of variables are assumed to be numeric. The ggplot2 plot object takes the form
 #'  of a color coded heatmap of the upper triangular portion of a symmetric correlation matrix.
 #'
-#' @param corr A square, symmetric, named, numeric matrix of correlation values between -1 to 1 in value. This is typically
-#'  the Value from \code{stats::cor()}.
+#' @param x A numeric data frame or matrix with variable columns and their observational values along the rows.
+#' @param create_plot A logical that if TRUE will create a heatmap plot of the correlations.
 #' @param cluster_corr A logical that if TRUE will cluster the correlations across the heatmap scales.
 #' @param tile_border_color A numeric that sets the heatmap tile's border color.
 #' @param label_sz A numeric that sets the size of the label.
@@ -27,13 +28,14 @@
 #' @importFrom RplotterPkg create_heatmap
 #' @import ggplot2
 #'
-#' @return A ggplot2 plot object.
+#' @return A named list with a data frame of correlations between the variables and a ggplot2 plot object.
 #'
 #' @author Rick Dean
 #'
 #' @export
 plot_correlations <- function(
-  corr,
+  x = NULL,
+  create_plot = TRUE,
   cluster_corr = FALSE,
   tile_border_color = "white",
   label_sz = 11,
@@ -51,55 +53,92 @@ plot_correlations <- function(
   legend_title = ggplot2::waiver(),
   show_legend = TRUE
 ){
-  corr <- round(corr,2)
-  if(cluster_corr){
-    hc <- stats::hclust(as.dist(1 - corr))
-    corr <- corr[hc$order, hc$order]
+  n <- nrow(x)
+
+  if(!anyNA(x)){
+    # compute variable means
+    means_v <- apply(X = x, MARGIN = 2, FUN = function(x){
+      mean(x)
+    })
+
+    # create a means for each column of variables
+    M <- matrix(means_v, nrow = 1)
+    means_mt <- matrix(data = 1, nrow = n) %*% M
+
+    # subtract the means from data
+    x_mt <- as.matrix(x)
+    diff_mt <- x_mt - means_mt
+
+    # create covariance matrix
+    var_cor_mt <- t(diff_mt) %*% diff_mt * (n - 1)^-1
+
+    # compute variables sd and sd cross products
+    var_mt <- diag(var_cor_mt)
+    sd_mt <- sqrt(var_mt)
+    sd_product_mt <- sd_mt %*% t(sd_mt)
+
+    # divide var-cor matrix by sd cross products
+    corr_mt <- round(var_cor_mt / sd_product_mt, digits = 2)
+
+    # create plot
+    if(create_plot){
+      if(cluster_corr){
+        hc <- stats::hclust(as.dist(1 - corr_mt))
+        corr_mt <- corr_mt[hc$order, hc$order]
+      }
+      corr_mt[lower.tri(corr_mt)] <- NA
+      corr_mt <- t(corr_mt)
+
+      dn_lst <- dimnames(corr_mt)
+      labels_df <- expand.grid(dn_lst, KEEP.OUT.ATTRS = FALSE)
+
+      missing_mat <- is.na(corr_mt)
+      data_v <- corr_mt[!missing_mat]
+      labels_df <- labels_df[!missing_mat, ]
+
+      values_df <- setNames(data.frame(data_v), "value")
+      df <- cbind(labels_df, values_df)
+
+      heatmap_plot <- RplotterPkg::create_heatmap(
+        df = df,
+        aes_x = "Var1",
+        aes_y = "Var2",
+        aes_fill = "value",
+        aes_label = "value",
+        tile_color = tile_border_color,
+        tile_square = TRUE,
+        label_sz = label_sz,
+        label_color = label_color,
+        label_fontface = label_fontface,
+        label_alpha = label_alpha,
+        title = title,
+        subtitle = subtitle,
+        center_titles = center_titles,
+        x_title = NULL,
+        y_title = NULL,
+        rot_x_tic_angle = rot_x_tic_angle,
+        rot_y_tic_label = TRUE,
+        axis_text_size = axis_text_size,
+        show_legend = show_legend
+      )
+
+      heatmap_plot <- heatmap_plot +
+        ggplot2::scale_fill_gradient2(
+          low = scale_colors[1],
+          high = scale_colors[2],
+          mid = mid_scale_color,
+          midpoint = 0,
+          limit = scale_limits,
+          name = legend_title
+        )
+      return(list(
+        corr = corr_mt,
+        corr_plot = heatmap_plot
+      ))
+    }else{
+      return(corr_mt)
+    }
+  }else {
+    stop("Numeric data frame/matrix must not have NA values")
   }
-  corr[lower.tri(corr)] <- NA
-  corr <- t(corr)
-
-  dn_lst <- dimnames(corr)
-  labels_df <- expand.grid(dn_lst, KEEP.OUT.ATTRS = FALSE)
-
-  missing_mat <- is.na(corr)
-  data_v <- corr[!missing_mat]
-  labels_df <- labels_df[!missing_mat, ]
-
-  values_df <- setNames(data.frame(data_v), "value")
-  df <- cbind(labels_df, values_df)
-
-  heatmap_plot <- RplotterPkg::create_heatmap(
-    df = df,
-    aes_x = "Var1",
-    aes_y = "Var2",
-    aes_fill = "value",
-    aes_label = "value",
-    tile_color = tile_border_color,
-    tile_square = TRUE,
-    label_sz = label_sz,
-    label_color = label_color,
-    label_fontface = label_fontface,
-    label_alpha = label_alpha,
-    title = title,
-    subtitle = subtitle,
-    center_titles = center_titles,
-    x_title = NULL,
-    y_title = NULL,
-    rot_x_tic_angle = rot_x_tic_angle,
-    rot_y_tic_label = TRUE,
-    axis_text_size = axis_text_size,
-    show_legend = show_legend
-  )
-
-  heatmap_plot <- heatmap_plot +
-    ggplot2::scale_fill_gradient2(
-      low = scale_colors[1],
-      high = scale_colors[2],
-      mid = mid_scale_color,
-      midpoint = 0,
-      limit = scale_limits,
-      name = legend_title
-    )
-  return(heatmap_plot)
 }
